5934e6a931dc8a6311a520b54cebc57d994ba687
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-08-01 12:20:41 -0700

    Modified queue_t struct and added init() function.

--- part2/pandc.c
+++ part2/pandc.c
@@ -18,7 +18,47 @@ typedef struct __queue_t {
     node_t *tail;
     pthread_mutex_t head_lock;
     pthread_mutex_t tail_lock;
+
+    size_t max_capacity;
+    size_t current_capacity;
+    size_t index;
+
+    node_t ** node_array;
 } queue_t;
+
+
+
+void init(queue_t *, size_t);
+int dequeue_item();
+int enqueue_item(int);
+
+
+
+int main(int argc, char * argv[])
+{
+
+    return 0;
+}
+
+
+
+void init(queue_t *q, size_t queue_size)
+{
+    node_t *tmp = malloc(sizeof(node_t));
+    tmp->value = 0;
+    tmp->next = NULL;
+
+    q->node_array = calloc(queue_size, sizeof(node_t *));
+    q->max_capacity = queue_size;
+    q->current_capacity = 0;
+    q->index = 0;
+
+    q->head = q->tail = tmp;
+    q->node_array[q->index] = tmp;
+
+    pthread_mutex_init(&q->head_lock, NULL);
+    pthread_mutex_init(&q->tail_lock, NULL);
+}
 
 /* 
  * Function to remove item.
@@ -26,7 +66,8 @@ typedef struct __queue_t {
  */
 int dequeue_item()
 {
-    
+
+    return 0;
 }
 
 /* 
@@ -40,9 +81,7 @@ int dequeue_item()
  */
 int enqueue_item(int item)
 {
-    
+    printf("Enqueued: %d\n", item);
+    return 0;
 }
 
-int main() {
-
-}
